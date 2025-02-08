af21e27acb2fb2d2230804efbab0078106a1d911
payumo-kg <126468607+callie102003@users.noreply.github.com>
2025-02-08 08:06:04 +0000

    commit

--- Rotting/routes/web.php
+++ Rotting/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/greet',[GreetController::class, 'showGreeting']);
+Route::resource('tasks', TaskController::class);
